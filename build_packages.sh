b6017c5f17000b1886db485ad5d4086d4102878f
Derek Calabro <dtcalabro@gmail.com>
2022-12-14 21:00:46 -0500

    Setup build packages script

--- build_packages.sh
+++ build_packages.sh
@@ -4,5 +4,5 @@ echo "[+] Building packages now!"
 dpkg-scanpackages -m ./debs > Packages
 cp Packages Packages.temp
 bzip2 Packages -f
-cp Packages.temp Packages
+mv Packages.temp Packages
 echo "[+] Done building packages!"
