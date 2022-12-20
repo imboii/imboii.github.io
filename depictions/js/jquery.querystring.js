c50fed4f6038f5d72cc1e89acc0548141d36eb07
Derek Calabro <dtcalabro@gmail.com>
2022-12-20 16:00:43 -0500

    Prettify jquery.querystring.js

--- depictions/js/jquery.querystring.js
+++ depictions/js/jquery.querystring.js
@@ -2,12 +2,11 @@
 	$.QueryString = (function(a) {
 		if (a == "") return {};
 		var b = {};
-		for (var i = 0; i < a.length; ++i)
-		{
-			var p=a[i].split('=');
+		for (var i = 0; i < a.length; ++i) {
+			var p = a[i].split('=');
 			if (p.length != 2) continue;
 			b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
 		}
 		return b;
-	})(window.location.search.substr(1).split('&'))
-})(jQuery);	
+	}) (window.location.search.substr(1).split('&'))
+}) (jQuery);
