5e152eaaf56dc050492d01512768bd64120babd7
Derek Calabro <dtcalabro@gmail.com>
2022-12-21 17:21:18 -0500

    Improve website styling

--- depictions/js/link-item-hover.js
+++ depictions/js/link-item-hover.js
@@ -1,16 +1,16 @@
 $(function() {
     console.log("link-item-hover.js debug message!");
-    var item = $('.link-item');
+    var item = $('.link-item.list-group-item');
     console.log(item);
     var orig_color = item.css("border-bottom");
     console.log(orig_color);
 
     item.hover(function() {
         //$('#b').css('background-color', 'yellow');
-        item.prev(".link-item").css("border-bottom", "3px solid white");
+        item.prev(".link-item.list-group-item").css("border-bottom", "3px solid white");
     }, function() {
         // on mouseout, reset the background colour
         //$('#b').css('background-color', '');
-        item.prev(".link-item").css("border-bottom", orig_color);
+        item.prev(".link-item.list-group-item").css("border-bottom", orig_color);
     });
 });
