eaa33b5d20b4c82ff6f324f6f8711e0fca1f7f57
Derek Calabro <dtcalabro@gmail.com>
2022-12-21 21:46:09 -0500

    Improve website styling

--- depictions/js/link-item-hover.js
+++ depictions/js/link-item-hover.js
@@ -1,16 +1,18 @@
-$(function() {
-    console.log("link-item-hover.js debug message!");
-    var item = $('.link-item.list-group-item');
-    console.log(item);
-    var orig_color = item.css("border-bottom");
-    console.log(orig_color);
+$(document).ready(function() {
+    $(function() {
+        console.log("link-item-hover.js debug message!");
+        var item = $('.link-item.list-group-item');
+        console.log(item);
+        var orig_color = item.css("border-bottom");
+        console.log(orig_color);
 
-    item.hover(function() {
-        //$('#b').css('background-color', 'yellow');
-        item.prev(".link-item.list-group-item").css("border-bottom", "3px solid white");
-    }, function() {
-        // on mouseout, reset the background colour
-        //$('#b').css('background-color', '');
-        item.prev(".link-item.list-group-item").css("border-bottom", orig_color);
+        item.hover(function() {
+            //$('#b').css('background-color', 'yellow');
+            item.prev(".link-item.list-group-item").css("border-bottom", "3px solid white");
+        }, function() {
+            // on mouseout, reset the background colour
+            //$('#b').css('background-color', '');
+            item.prev(".link-item.list-group-item").css("border-bottom", orig_color);
+        });
     });
 });
